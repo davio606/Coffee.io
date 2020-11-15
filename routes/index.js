a424dd9b066976db937265821059b4a98f1a071d
Andrew Murphy <afm9yy@virginia.edu>
2020-11-15 12:59:02 -0500

    update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const connection = require('../database');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', user: req.user });
+router.get('/', function (req, res, next) {
+  if (req.user) {
+    console.log('yo')
+    connection.query(
+      "SELECT * FROM `User` WHERE email = ?", req.user.email,
+      function (error, results, fields) {
+        if (error) throw error;
+        res.render('index', { title: 'Express', user: req.user, userdata: results[0] });
+      }
+    );
+  }
+  else {
+    res.render('index', { title: 'Express', user: req.user });
+  }
 });
 
 module.exports = router;
