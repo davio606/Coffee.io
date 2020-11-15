c0fdeb73df40fa7c575ca8edc11ac7286fe24893
Andrew Murphy <afm9yy@virginia.edu>
2020-11-14 20:58:44 -0500

    find page updated

--- routes/find.js
+++ routes/find.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const connection = require('../database');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('find', { title: 'Express' });
+  connection.query(
+    "SELECT * FROM `Coffeeshop` ",
+    function(error, results, fields) {
+      if (error) throw error;
+      res.render('find', { title: 'Express', shops: results});
+    }
+  );
 });
 
 module.exports = router;
