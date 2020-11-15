9c2808e69a8955770f0406912c65c550ed268d08
davio606 <djy6cg@virginia.edu>
2020-11-15 11:21:09 -0500

    Working on query for menu

--- routes/bakery.js
+++ routes/bakery.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('bakery', { title: 'Express' });
+  res.render('bakery', { title: 'Express'});
 });
 
 module.exports = router;
--- routes/order.js
+++ routes/order.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const connection = require('../database');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('order', { title: 'Express' });
+  var name = req.query.name;
+  var id = parseInt(req.query.shopid);
+  connection.query(
+    "SELECT * FROM 'MenuItem' WHERE shopID = ?", id,
+    function(error, results, fields) {
+      if (error) throw error;
+      res.render('find', { title: 'Express', name:name, items: results});
+    }
+  );
 });
-
+console.log("here again");
 module.exports = router;
